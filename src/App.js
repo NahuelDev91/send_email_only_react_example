f16f324532bf00d7d50f935f6228a4a6894de2c3
nahuel.jotandjota <nahuel.dev.91@gmail.com>
2022-02-13 10:20:43 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import { Meiler } from './components/Meiler';
+
+function App() {
+  return (
+    <div className="App">
+     <Meiler/>
+    </div>
+  );
+}
+
+export default App;
